302c672a12ef9087b78f2db615608218e0eb8c41
Renako223678 <134682228+Renako223678@users.noreply.github.com>
2024-10-06 14:02:22 +0700

    add authorize to swagger

--- SWD392_KoiOrderingSystemInJapan/KoiOrderingSystemInJapan.APIService/Program.cs
+++ SWD392_KoiOrderingSystemInJapan/KoiOrderingSystemInJapan.APIService/Program.cs
@@ -2,6 +2,7 @@
 using KoiOrderingSystemInJapan.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Encodings.Web;
 
@@ -17,6 +18,8 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.PropertyNamingPolicy = null; // Giữ nguyên tên thuộc tính
         options.JsonSerializerOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping; // Cho phép ký tự đặc biệt
     });
+
+
 //Add authentication configure 
 builder.Services.AddAuthentication(options =>
 {
@@ -36,6 +39,43 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Koi Ordering System in Japan",
+        Version = "v1"
+    });
+
+    // Add JWT Authentication to Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Example: 'Bearer {token}'"
+    });
+
+    // Add the security requirement for JWT Bearer authentication
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -53,7 +93,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "User Authentication API");
+          // Set Swagger to open at the root URL
+    });
 }
 
 app.UseHttpsRedirection();
