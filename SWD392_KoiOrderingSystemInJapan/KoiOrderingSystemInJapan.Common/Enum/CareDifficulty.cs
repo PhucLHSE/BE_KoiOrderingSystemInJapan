595c5b6cb22c01b2e4f468ebbd342b914d09a8c5
Huynh Nguyen Ngoc Nguyen - N1 <Nguyen1.24112003@gmail.com>
2024-10-20 19:07:19 +0700

    enum

---
+++ SWD392_KoiOrderingSystemInJapan/KoiOrderingSystemInJapan.Common/Enum/CareDifficulty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiOrderingSystemInJapan.Common.Enum
+{
+    public enum CareDifficulty
+    {
+        Low,
+        Medium, 
+        High,
+        Special
+    }
+}
