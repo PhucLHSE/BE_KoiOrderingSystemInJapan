b1e8a85c0cf141ae50e4e77d846d54264aca3cce
PhucLHSE <phuclhse173083@fpt.edu.vn>
2024-11-08 12:23:29 +0700

    Update Database and Fix Backend

--- SWD392_KoiOrderingSystemInJapan/KoiOrderingSystemInJapan.Data/DBContext/KoiOrderingSystemInJapanContext.cs
+++ SWD392_KoiOrderingSystemInJapan/KoiOrderingSystemInJapan.Data/DBContext/KoiOrderingSystemInJapanContext.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using KoiOrderingSystemInJapan.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace KoiOrderingSystemInJapan.Data.DBContext;
 
@@ -52,9 +53,22 @@ public partial class KoiOrderingSystemInJapanContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
+    public static string GetConnectionString(string connectionStringName)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string connectionString = config.GetConnectionString(connectionStringName);
+        return connectionString;
+    }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=ADMIN-PC;Initial Catalog=KoiOrderingSystemInJapan;Persist Security Info=True;User ID=sa;Password=12345;Encrypt=False");
+        => optionsBuilder.UseSqlServer(GetConnectionString("DefaultConnection")).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    //        => optionsBuilder.UseSqlServer("Data Source=ADMIN-PC;Initial Catalog=KoiOrderingSystemInJapan;Persist Security Info=True;User ID=sa;Password=12345;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
